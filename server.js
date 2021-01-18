9e07b1eb6da9b2a1706bc5c5e7750df408280bda
sarra <saraalsara@hotmail.com>
2021-01-18 11:35:09 +0300

    clean file

--- server.js
+++ server.js
@@ -9,14 +9,13 @@ const app = express();
 
 /* Dependencies */
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
-const cors = require('cors');
 app.use(cors());
 
 // Initialize the main project folder
@@ -26,11 +25,6 @@ app.use(express.static('website'));
 app.get('/all', (req, res) => {
     res.send(projectData)
 })
-
-// // Callback function to complete GET '/all'
-// function getData(req, res) {
-//     res.send(projectData)
-// }
 
 // Post Route
 app.post('/addWeatherData', addData)
@@ -42,17 +36,13 @@ function addData(req, res) {
     }
     projectData.push(newEntry)
     res.send(projectData)
-    console.log(projectData,"project Data")
+    console.log(projectData, "project Data")
 }
 // Setup Server
 const port = 8000;
 
-//spin up the server
+//Spin up the server
 app.listen(port, () => {
-    console.log(`server is running on http://localhost ${port}`)
+    console.log(`server is running on http://localhost:${port}`)
 })
-// Callback to debug
-// function listening() {
-//     console.log("server is running");
-//     console.log(`running on localhost on port ${port}`)
-// }
+
